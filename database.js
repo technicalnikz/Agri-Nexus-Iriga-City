1b816994cb2cf91309b9b8185805946c3991b9e8
agravante_project-admin <anagravante@my.cspc.edu.ph>
2026-05-14 23:31:31 +0800

    refactor: update PostgreSQL database connector to use persistent client for transaction support

--- database.js
+++ database.js
@@ -5,51 +5,65 @@ const path = require('path');
 let db;
 
 if (process.env.DATABASE_URL) {
-    // PostgreSQL Configuration (for Render)
     console.log('Connecting to PostgreSQL database...');
-    db = new Pool({
+    const pool = new Pool({
         connectionString: process.env.DATABASE_URL,
-        ssl: {
-            rejectUnauthorized: false // Required for Render/ElephantSQL
-        }
+        ssl: { rejectUnauthorized: false }
     });
 
-    // Mock the SQLite 'all', 'get', 'run', 'serialize' methods for compatibility
-    // Note: This is a bridge to avoid rewriting every line in server.js immediately,
-    // but a full async refactor is better.
+    // Use a single client for everything to support Transactions (BEGIN/COMMIT)
+    // which require the same connection/session.
+    let pgClient = null;
+    const getClient = async () => {
+        if (!pgClient) {
+            pgClient = await pool.connect();
+        }
+        return pgClient;
+    };
+
+    db = {};
     db.serialize = (cb) => cb();
 
-    const originalQuery = db.query.bind(db);
-
-    db.all = (sql, params, callback) => {
-        // Convert ? placeholders to $1, $2 style
-        let i = 1;
-        const pgSql = sql.replace(/\?/g, () => `$${i++}`);
-        originalQuery(pgSql, params)
-            .then(res => callback(null, res.rows))
-            .catch(err => callback(err));
+    db.query = async (sql, params) => {
+        const client = await getClient();
+        return client.query(sql, params);
     };
 
-    db.get = (sql, params, callback) => {
-        let i = 1;
-        const pgSql = sql.replace(/\?/g, () => `$${i++}`);
-        originalQuery(pgSql, params)
-            .then(res => callback(null, res.rows[0]))
-            .catch(err => callback(err));
+    db.all = async (sql, params, callback) => {
+        try {
+            const client = await getClient();
+            let i = 1;
+            const pgSql = sql.replace(/\?/g, () => `$${i++}`);
+            const res = await client.query(pgSql, params);
+            if (callback) callback(null, res.rows);
+        } catch (err) {
+            if (callback) callback(err);
+        }
     };
 
-    db.run = function (sql, params, callback) {
-        let i = 1;
-        const pgSql = sql.replace(/\?/g, () => `$${i++}`);
-        originalQuery(pgSql, params)
-            .then(res => {
-                // For PostgreSQL, we get the ID from the first row of results if RETURNING is used
-                const lastID = res.rows && res.rows[0] ? Object.values(res.rows[0])[0] : (res.oid || null);
-                if (callback) callback.call({ lastID: lastID, changes: res.rowCount }, null);
-            })
-            .catch(err => {
-                if (callback) callback(err);
-            });
+    db.get = async (sql, params, callback) => {
+        try {
+            const client = await getClient();
+            let i = 1;
+            const pgSql = sql.replace(/\?/g, () => `$${i++}`);
+            const res = await client.query(pgSql, params);
+            if (callback) callback(null, res.rows[0]);
+        } catch (err) {
+            if (callback) callback(err);
+        }
+    };
+
+    db.run = async function (sql, params, callback) {
+        try {
+            const client = await getClient();
+            let i = 1;
+            const pgSql = sql.replace(/\?/g, () => `$${i++}`);
+            const res = await client.query(pgSql, params);
+            const lastID = res.rows && res.rows[0] ? Object.values(res.rows[0])[0] : (res.oid || null);
+            if (callback) callback.call({ lastID: lastID, changes: res.rowCount }, null);
+        } catch (err) {
+            if (callback) callback(err);
+        }
     };
 } else {
     // SQLite Configuration (for Local Development)
